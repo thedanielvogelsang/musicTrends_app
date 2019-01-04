28ef33d91353b4d36af2c3ac3eb50f589ba0ebff
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-01-04 11:36:46 -0700

    readies routes page with /search /song/:id to deliver main page search results and exact song given id via API version 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+    get 'search_songs', to: "api/v1/songs_controller#index"
+    get 'songs/:song_id', to: "api/v1/songs_controller#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+    end
+  end
 end
