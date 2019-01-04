ced3aa500c577330cfc93251524e71e7a4db15c4
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-01-04 10:38:58 -0700

    adds has_many :searches to Song model with passing association tests

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,5 @@
 class Song < ApplicationRecord
   validates_presence_of :id
+  has_many :song_searches
+  has_many :searches, through: :song_searches
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Song, type: :model do
   context "validations" do
-    it{should validate_presence_of(:id)}
+    it {should validate_presence_of(:id)}
   end
   context "associations" do
-
+    it {should have_many(:song_searches)}
+    it {should have_many(:searches)}
   end
 end
