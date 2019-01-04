7b2ca922935e6c311d3b4aa9ff104f06fb0314c1
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-01-04 12:37:21 -0700

    adds logic to songs_controller_spec that corresponds with GeniusService returns (nil vs array vs hash) and tests accordingly; ready for pagination!

--- spec/controllers/api/v1/songs_controller_spec.rb
+++ spec/controllers/api/v1/songs_controller_spec.rb
@@ -14,17 +14,26 @@ RSpec.describe Api::V1::SongsController, type: :controller do
       VCR.use_cassette "/cassettes/controllers/chicago" do
         get :index, params: {query: "Chicago"}
         expect(response).to have_http_status(:success)
-        expect(JSON.parse(response.body).length).to eq(20)
+      end
+    end
+    it "#with query: returns results paginated at 10 per_page" do
+      VCR.use_cassette("/cassettes/controllers/chicago", :allow_playback_repeats => true) do
+        query = "Chicago"
+        resp = GeniusService.new.get_songs(query)
+        get :index, params: {query: query}
+        expect(response).to have_http_status(:success)
+        expect(JSON.parse(response.body).length).to eq(10)
+        expect(resp.length).to eq(20)
       end
     end
   end
 
   describe "GET #show" do
-    it "returns http failure without song id" do
-      VCR.use_cassette "/cassettes/services/chicago_25or624" do
+    it "returns http success with errored song id" do
+      VCR.use_cassette "/cassettes/services/failed_song_id" do
         get :show, params: {song_id: 0}
-        expect(response).to have_http_status(404)
-        expect(JSON.parse(response.body)['error']).to eq("Try a different query, that one's stale")
+        expect(response).to have_http_status(202)
+        expect(JSON.parse(response.body)).to eq(nil)
       end
     end
     it "returns http success with song id" do
