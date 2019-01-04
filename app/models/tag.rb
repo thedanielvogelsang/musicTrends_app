cf830442e6c74273a0e2182f27a9d22ff1980ec7
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-01-04 12:56:05 -0700

    basic validation and association tests for tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ApplicationRecord
+  validates_presence_of :context
+  validates_presence_of :key_words
   has_many :keyword_taggings
   has_many :keywords, through: :keyword_taggings
 end
