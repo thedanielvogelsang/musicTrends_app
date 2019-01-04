3a8c845a109c33f8c075773864b9992b45794f83
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-01-04 11:29:39 -0700

    genius_api return JSON parsed returns in form of array, confirmed match for songs

--- spec/services/genius_service_spec.rb
+++ spec/services/genius_service_spec.rb
@@ -8,11 +8,27 @@ RSpec.describe GeniusService do
     end
   end
   context "utility" do
-    it "#get_songs can get songs given a query" do
+    it "#get_songs can get list of songs given a query" do
       VCR.use_cassette "/cassettes/services/love" do
         query = "Love"
         api_return = GeniusService.new.get_songs(query)
         assert_equal(20, api_return.length)
+      end
+    end
+    it "#get_songs returns as array" do
+      VCR.use_cassette "/cassettes/services/love" do
+        query = "Love"
+        api_return = GeniusService.new.get_songs(query)
+        assert_equal(api_return.class, Array)
+      end
+    end
+    it "#get_songs returns list of songs" do
+      VCR.use_cassette "/cassettes/services/love" do
+        query = "Love"
+        api_return = GeniusService.new.get_songs(query)
+
+        expect(/(\/songs)/.match(api_return.first[:api_path])).to be_truthy
+        expect(/(\/artists)/.match(api_return.first[:api_path])).to be nil
       end
     end
   end
