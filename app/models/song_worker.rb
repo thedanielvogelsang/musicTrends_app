e671141a06b882ba9904bd4d21e9e636ef68e333
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-01-04 18:23:43 -0700

    song worker ready to rumble, all tests passing and get_referents properly returning list of annotations

--- app/models/song_worker.rb
+++ app/models/song_worker.rb
@@ -4,6 +4,11 @@ class SongWorker
   def initialize(song_id)
     @song_id = song_id
     @ref = GeniusService.new.get_referents(song_id)
+    sanitize_api_return
   end
 
+  private
+    def sanitize_api_return
+      @ref = @ref.map{|r|}
+    end
 end
