9dcebd50f1adffab727ad7a55f15e097159268d0
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-01-05 11:03:16 -0700

    updates song_worker_spec and song_worker to break out #get_referents to separate instance method, not in initialize. Songworker more flexible now

--- app/models/song_worker.rb
+++ app/models/song_worker.rb
@@ -2,17 +2,22 @@ class SongWorker
   include Words
   attr_reader :song_id, :refs
 
-  def initialize(song_id)
+  def initialize(song_id, refs = nil)
     @song_id = song_id
-    @refs = GeniusService.new.get_referents(song_id)
+    @refs = refs
   end
 
-  def update_or_create_word_list
-    song = Song.find(@song_id)
+  def get_referents
+    @refs = GeniusService.new.get_referents(song_id)
+    return self
+  end
+
+  def update_or_create_word_dict
+    song = Song.find(song_id)
     word_count = song.word_dict
-    @refs.each do |ref|
+    refs.each do |ref|
       ref = ref[:annotations][0][:body][:dom][:children][0][:children]
-      wrd_cnt = WordCounter.new("Song", @song_id, ref).count_words
+      wrd_cnt = WordCounter.new("Song", song_id, ref).count_words
       wrd_cnt.each do |k, v|
         word_count[k] ? word_count[k] += v : word_count[k] = v
       end
@@ -21,18 +26,23 @@ class SongWorker
     song.save
   end
 
+  def get_referents_and_update_word_dict
+    get_referents
+    update_or_create_word_dict
+  end
+
   def find_trends
     save_highfreq_words_as_keywords
   end
 
   def save_highfreq_words_as_keywords
-    freq_words_in_dict = Song.find(@song_id)
+    freq_words_in_dict = Song.find(song_id)
                               .word_dict
                               .select{|k,v| v.to_i > 3}
     freq_words_in_dict.keys.each do |frqword|
       if !CommonWords::WORDS.include?(frqword)
         k = Keyword.find_or_create_by(phrase: frqword)
-        KeywordSongMatch.search_and_add(k.id, @song_id)
+        KeywordSongMatch.search_and_add(k.id, song_id)
       end
     end
   end
--- spec/workers/song_worker_spec.rb
+++ spec/workers/song_worker_spec.rb
@@ -12,20 +12,23 @@ RSpec.describe SongWorker, type: :model do
       )
     end
     it "can return song_id as attr_reader" do
-      VCR.use_cassette "/workers/britney_song_worker" do
         swork = SongWorker.new(@song.id)
         expect(swork.song_id).to eq(@song.id)
-      end
     end
     it "can return ref_list as attr_reader" do
       VCR.use_cassette "/workers/britney_song_worker" do
         swork = SongWorker.new(@song.id)
+        swork.get_referents
+        expect(swork.refs).to be_truthy
         expect(swork.refs.class).to eq(Array)
       end
     end
-    it "initializes with an API call to find referent annotations" do
+    it "initializes without an API call to find referent annotations" do
       VCR.use_cassette "/workers/britney_song_worker" do
         sworker = SongWorker.new(@song.id)
+        assert_nil(sworker.refs)
+        sworker.get_referents
+        expect(sworker.refs).to be_truthy
         expect(sworker.refs.class).to eq(Array)
         expect(sworker.refs.length).to eq(10)
       end
@@ -45,9 +48,18 @@ RSpec.describe SongWorker, type: :model do
       VCR.use_cassette "/workers/britney_song_worker" do
         hash = @song.word_dict
         expect(hash.empty?).to be true
-        SongWorker.new(@song.id).update_or_create_word_list
+        sw = SongWorker.new(@song.id).get_referents
+        sw.update_or_create_word_dict
         song = Song.find(@song.id)
         expect(hash).to_not eq(song.word_dict)
+        expect(song.word_dict.empty?).to be false
+        expect(song.word_dict.length).to eq(117)
+      end
+    end
+    it "can do the same with an integrated method call" do
+      VCR.use_cassette "/workers/britney_song_worker" do
+        sw = SongWorker.new(@song.id).get_referents_and_update_word_dict
+        song = Song.find(@song.id)
         expect(song.word_dict.empty?).to be false
         expect(song.word_dict.length).to eq(117)
       end
@@ -55,8 +67,8 @@ RSpec.describe SongWorker, type: :model do
     it "#save_highfreq_words_as_keywords saves key words as Keywords" do
       VCR.use_cassette "/workers/britney_song_worker" do
         expect(Keyword.count).to eq(0)
-        sw = SongWorker.new(@song.id)
-        sw.update_or_create_word_list
+        sw = SongWorker.new(@song.id).get_referents
+        sw.update_or_create_word_dict
         sw.save_highfreq_words_as_keywords
         expect(Keyword.count > 0).to be true
       end
@@ -64,8 +76,8 @@ RSpec.describe SongWorker, type: :model do
     it "#save_highfreq_words_as_keywords also links song and keyword together" do
       VCR.use_cassette "/workers/britney_song_worker" do
         expect(@song.keywords.empty?).to be true
-        sw = SongWorker.new(@song.id)
-        sw.update_or_create_word_list
+        sw = SongWorker.new(@song.id).get_referents
+        sw.update_or_create_word_dict
         sw.save_highfreq_words_as_keywords
         expect(@song.keywords.empty?).to be false
       end
