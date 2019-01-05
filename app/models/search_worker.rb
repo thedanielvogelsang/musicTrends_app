907b3c02594567f3783492bbffe290786ffd5725
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-01-05 16:18:49 -0700

    search_worker basic tests passing; searches added via id will have their words assembled into keywords

--- app/models/search_worker.rb
+++ app/models/search_worker.rb
@@ -1,12 +1,31 @@
 class SearchWorker
   include Words
-  attr_reader :search_id, :refs
+  attr_reader :search_id, :ref
 
   def initialize(search_id)
     @search_id = Search.find(search_id).id
     rescue ActiveRecord::RecordNotFound => e
-        raise ArgumentError, 'Argument is not numeric'
-    @refs = nil 
+        raise ArgumentError, 'Argument is not numeric and/or Record cannot be found'
+    @ref = nil
+  end
+
+  def clean_and_separate_words
+    @ref = Search.find(search_id).text
+    clean_words
+  end
+
+  def clean_words
+    @ref = @ref.gsub(/[^a-zA-Z0-9 ]/, "").split(' ').map{|w| w.downcase}.join(' ').strip
+  end
+
+  def create_keywords
+    clean_and_separate_words
+    @ref.split(' ').each do |word|
+      unless CommonWords::WORDS.include?(word)
+        k = Keyword.find_or_create_by(phrase: word)
+        KeywordSearchMatch.search_and_add(k.id, search_id)
+      end
+    end
   end
 
 end
--- spec/workers/search_worker_spec.rb
+++ spec/workers/search_worker_spec.rb
@@ -9,9 +9,25 @@ RSpec.describe SearchWorker, type: :model do
         text: "Won the grammys last year"
       )
     end
+    it "can call ref" do
+      expect(SearchWorker.new(@search.id).clean_and_separate_words).to eq("won the grammys last year")
+    end
     it "SearchWorker initializes with existing search_id" do
       expect{ SearchWorker.new(0)}.to raise_error(ArgumentError)
       expect{ SearchWorker.new(@search.id)}.to_not raise_error
+    end
+  end
+  describe "SearchWorker can create keywords given search id" do
+    before(:each) do
+      @search = Search.create(
+        search_type: "Song",
+        text: "Won the grammys last year"
+      )
+    end
+    it "creates new keywords" do
+      expect(Keyword.count).to eq(0)
+      SearchWorker.new(@search.id).create_keywords
+      expect(Keyword.count).to eq(4)
     end
   end
 end
