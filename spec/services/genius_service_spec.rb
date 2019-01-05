10ea4da715a80a6e888e765c731fe93574a15a72
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-01-04 18:42:25 -0700

    bulked up SongWorker discussion on referents and double checked api return for important/relevant data... none to be found other than authors of comments' metadata

--- spec/services/genius_service_spec.rb
+++ spec/services/genius_service_spec.rb
@@ -60,6 +60,9 @@ RSpec.describe GeniusService do
           assert_equal("referent",
                         api_return.first[:_type]
                       )
+          assert_equal("referent",
+                        api_return.last[:_type]
+                      )
           assert_equal(String,
                         api_return.last[:annotations][0][:body][:dom][:children][0][:children].join("").class
                       )
@@ -69,6 +72,43 @@ RSpec.describe GeniusService do
       VCR.use_cassette "/services/get_referents_failed_song_id" do
           api_return = GeniusService.new.get_referents(0)
           assert_nil(api_return)
+      end
+    end
+    describe "annotation object" do
+      it "has many great pieces of info" do
+        VCR.use_cassette "/services/britney" do
+          api_return = GeniusService.new.get_referents(@song_id)
+          assert_equal(Array, api_return.class)
+          # annotations stemming from just one referent
+          mapped_annotations = api_return.last[:annotations][0][:body][:dom][:children]
+                                        .map{|ann| ann }
+
+          assert_equal(3, mapped_annotations.length)
+          assert_equal("referent",
+                        api_return.first[:_type]
+                      )
+          # hashes, array, and strings returned as sub_elements of :children key
+          mapped_classes = mapped_annotations.map{|ann| ann.class }
+          assert(mapped_classes.include?(Hash))
+          assert(mapped_classes.include?(String))
+
+          # assert_equal(String,
+          #   api_return.last[:annotations][0][:body][:dom][:children][0][:children].join("").class
+          # )
+          assert_equal("Great guitar Wah wah solo by Terry Kath. Much longer in the album version than in the single.",
+            api_return.last[:annotations][0][:body][:dom][:children][0][:children].join("").gsub(/[^a-zA-Z0-9\.\- ]/, "")
+          )
+          assert_equal("",
+            api_return.last[:annotations][0][:body][:dom][:children][1]
+          )
+          assert_equal("The solo shows off how fast Kath can play. While Kath took guitar lessons for a short time, he quit and taught himself guitar, saying all he wanted to do was “play those rock and roll guitar chords.”",
+            api_return.last[:annotations][0][:body][:dom][:children][2][:children].join("")
+          )
+          # returns info on authors too
+          assert_equal(Array,
+            api_return.last[:annotations][0][:authors].class
+          )
+        end
       end
     end
   end
