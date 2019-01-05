7893f830c9588347b345a16e0e4131dadfcde19a
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-01-04 18:43:36 -0700

    removes unnecessary sanitization method in SongWorker

--- app/models/song_worker.rb
+++ app/models/song_worker.rb
@@ -4,11 +4,5 @@ class SongWorker
   def initialize(song_id)
     @song_id = song_id
     @ref = GeniusService.new.get_referents(song_id)
-    sanitize_api_return
   end
-
-  private
-    def sanitize_api_return
-      @ref = @ref.map{|r|}
-    end
 end
