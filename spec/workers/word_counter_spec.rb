39a5cfa1d917745d6da378456aa0705692bed74d
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-01-05 13:55:33 -0700

    WordCounter now can be used for simply #count_words instance method

--- spec/workers/word_counter_spec.rb
+++ spec/workers/word_counter_spec.rb
@@ -24,8 +24,8 @@ RSpec.describe WordCounter, type: :model do
         wcount = word_counter.count_words
         expect(wcount).to be_instance_of(Hash)
         expect(wcount.length).to eq(3)
-        expect(wcount.keys).to eq(['word1', 'word2', 'another.word'])
-        expect(wcount).to eq({'word1' => 2, 'word2' => 2, 'another.word' => 1})
+        expect(wcount.keys).to eq(['word1', 'word2', 'anotherword'])
+        expect(wcount).to eq({'word1' => 2, 'word2' => 2, 'anotherword' => 1})
       end
       it "refs are totalled accurately even with odd annotations" do
         word_counter = WordCounter.new('Song', 4, ["word1", {:tag => 2}, "word2 word1 another.word", ['word'], 'word2'+'word1'])
@@ -33,8 +33,17 @@ RSpec.describe WordCounter, type: :model do
         wcount = word_counter.count_words
         expect(wcount).to be_instance_of(Hash)
         expect(wcount.length).to eq(4)
-        expect(wcount.keys).to eq(['word1', 'word2', 'another.word', 'word2word1'])
-        expect(wcount).to eq({'word1' => 2, 'word2' => 1, 'another.word' => 1, 'word2word1' => 1})
+        expect(wcount.keys).to eq(['word1', 'word2', 'anotherword', 'word2word1'])
+        expect(wcount).to eq({'word1' => 2, 'word2' => 1, 'anotherword' => 1, 'word2word1' => 1})
+      end
+    end
+    context "instance methods" do
+      it "without type or id can return word_count from string" do
+        words = "I don't care betty, you don't have a right! Betty!"
+        word_Count = WordCounter.new(nil, nil, words).count_words
+        expect(word_Count['betty']).to eq(2)
+        expect(word_Count['dont']).to eq(2)
+        expect(Keyword.count).to eq(0)
       end
     end
   end
