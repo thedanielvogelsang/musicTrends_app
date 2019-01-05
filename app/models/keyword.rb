b2f08b27d6102188f13a8b8958115a6b4129eedb
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-01-05 16:21:13 -0700

    keywords linked and dependencies destroyed for keyword_search/song_matches; search_worker done for now

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -2,7 +2,8 @@ class Keyword < ApplicationRecord
   validates :phrase, presence: true, uniqueness: true
   has_many :keyword_taggings, dependent: :destroy
   has_many :tags, through: :keyword_taggings
-
+  has_many :keyword_song_matches, dependent: :destroy
+  has_many :keyword_search_matches, dependent: :destroy
   before_validation :downcase_phrase
 
   def downcase_phrase
