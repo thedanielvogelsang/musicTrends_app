4732af7158d92a66442d8ab9cf6d411edb8b142e
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-01-04 21:10:34 -0700

    odd annotation list for Song entry

--- spec/workers/word_counter_spec.rb
+++ spec/workers/word_counter_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe WordCounter, type: :model do
         word_counter = WordCounter.new('Song', 4, @refs)
         expect(word_counter.ref).to be_instance_of(String)
       end
-      it "refs are totalled accurately with count_words" do
+      it "refs are totalled accurately with #count_words" do
         word_counter = WordCounter.new('Song', 4, ["word1", "word2", "word2 word1 another.word"])
         expect(word_counter.ref).to be_instance_of(String)
         wcount = word_counter.count_words
@@ -26,6 +26,15 @@ RSpec.describe WordCounter, type: :model do
         expect(wcount.length).to eq(3)
         expect(wcount.keys).to eq(['word1', 'word2', 'another.word'])
         expect(wcount).to eq({'word1' => 2, 'word2' => 2, 'another.word' => 1})
+      end
+      it "refs are totalled accurately even with odd annotations" do
+        word_counter = WordCounter.new('Song', 4, ["word1", {:tag => 2}, "word2 word1 another.word", ['word'], 'word2'+'word1'])
+        expect(word_counter.ref).to be_instance_of(String)
+        wcount = word_counter.count_words
+        expect(wcount).to be_instance_of(Hash)
+        expect(wcount.length).to eq(4)
+        expect(wcount.keys).to eq(['word1', 'word2', 'another.word', 'word2word1'])
+        expect(wcount).to eq({'word1' => 2, 'word2' => 1, 'another.word' => 1, 'word2word1' => 1})
       end
     end
   end
