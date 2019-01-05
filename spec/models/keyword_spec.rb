a0ab5cca30724103261258f6ba24b549b6570528
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-01-04 17:23:45 -0700

    updates keyword_spec and adds code to song_tagging_spec

--- spec/models/keyword_spec.rb
+++ spec/models/keyword_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Keyword, type: :model do
   context "validations" do
-    it{should validate_presence_of(:context)}
-    it{should validate_presence_of(:key_words)}
+    it{should validate_presence_of(:phrase)}
   end
   context "associations" do
     it{should have_many(:keyword_taggings)}
-    it{should have_many(:keywords)}
+    it{should have_many(:tags)}
   end
 end
