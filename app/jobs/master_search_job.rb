1ffd8aeb25cd6d7432cbd0c7c1732a91f1c8f01d
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-01-05 17:16:35 -0700

    master_search_job off the ground with tests

---
+++ app/jobs/master_search_job.rb
@@ -0,0 +1,18 @@
+class MasterSearchJob
+  include Sidekiq::Worker
+
+  def perform(song_params, search_params)
+    begin
+    song = Song.find_or_create_by(song_params)
+    rescue
+      raise ActiveRecord::RecordNotFound
+    end
+    begin
+    search = Search.find_or_create_by(search_params)
+    rescue
+      raise ActiveRecord::RecordNotFound
+    end
+    song = Song.find_or_create_by(song_params)
+    search = Search.find_or_create_by(search_params)
+  end
+end
