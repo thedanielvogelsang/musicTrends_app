1a60adad95daf9e1a8da58de8416c42353808983
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-01-05 18:23:48 -0700

    master_search_job_spec is about to get reaaalllll......

--- spec/jobs/master_search_job_spec.rb
+++ spec/jobs/master_search_job_spec.rb
@@ -25,6 +25,21 @@ RSpec.describe "MasterSearchJob", :type => :job do
         expect(Song.count).to eq(1)
         expect(Search.count).to eq(1)
       end
+      it "triggers an association between song and search" do
+        song = Song.create(id: 28660)
+        search = Search.create(search_type: "Song", text: "Whats the name of the band that plays 'Whatever'?")
+        expect(song.searches.count).to eq(0)
+        expect(search.songs.count).to eq(0)
+
+        song_params = {id: 28660, artist_id: 1230586, artist_name: "Artist 1", title: "Whatever", annotation_ct: 0}
+        search_params = {text: "Whats the name of the band that plays 'Whatever'?", search_type: "Song"}
+        MasterSearchJob.perform_async(song_params, search_params)
+        expect{Sidekiq::Worker.drain_all}.to_not raise_error
+        expect(Song.count).to eq(1)
+        expect(Search.count).to eq(1)
+        expect(Song.first.searches.count).to eq(1)
+        expect(Song.first.searches.first).to eq(Search.first)
+      end
     end
   end
 end
