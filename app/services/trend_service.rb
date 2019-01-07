942d1c78c57990b275dc5ee6b5b24e9765c1da2e
thedanielvogelsang <dr.vogelsang26@gmail.com>
2019-01-06 18:37:48 -0700

    adds trends_service to app to connect with aws, pending format (which ill have to check manually), we should be set to test an actual object formation!

--- app/services/trend_service.rb
+++ app/services/trend_service.rb
@@ -1,11 +1,18 @@
-class TrendsService
+class TrendService
   attr_reader :conn
-  
-  def initialize
+
+  def initialize(type_of_hash, hsh_info)
+    @type = type_of_hash
+    @hsh_info = hsh_info
     @conn = Aws::S3::Client.new(
       credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
       region: ENV['S3_REGION'],
     )
   end
 
+  def log_song_trends
+    body = []
+    body.push(@hsh_info.to_a.join(',').strip)
+    
+  end
 end
--- spec/services/trends_service_spec.rb
+++ spec/services/trends_service_spec.rb
@@ -1,17 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe TrendsService do
+RSpec.describe TrendService do
   before(:each) do
   end
   describe "intialization" do
     it "requires type and param to generate log" do
-      expect{TrendsService.new}.to raise_error(ArgumentError)
+      expect{TrendService.new}.to raise_error(ArgumentError)
       expect{TrendService.new("Song", {})}.to_not raise_error
     end
     it "initializes with a connection" do
-      aws = TrendsService.new("Song", {})
-      expect(aws.conn.class?).to eq(AWS::Client::Connection)
+      aws = TrendService.new("Song", {})
+      expect(aws.conn.class).to eq(Aws::S3::Client)
     end
   end
-
+  describe "utility" do
+    it "can put an object to aws console" do
+      
+    end
+  end
 end
